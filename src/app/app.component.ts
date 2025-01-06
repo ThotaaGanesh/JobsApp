e00208cc4dc36693be19d89433e8c2249d65a5a8
Ganesh Thota <ganesh_thota@epam.com>
2025-01-06 16:47:49 +0530

    Added bootstrap files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+declare var $: any; // Declare jQuery so TypeScript recognizes $
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,5 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'my-jobs-app';
+ 
 }
